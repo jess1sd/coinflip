67c75d768934346558bff461cb051784bfaff9d3
jchiang <jessica.chiang@sony.com>
2021-07-21 07:56:08 -0700

    add files

---
+++ flip_step4.py
@@ -0,0 +1,8 @@
+import constant
+# define a variable 
+random_val = 0.50;
+if (random_val> 0.50):
+    print(constant.HEAD)
+else:
+    print(constant.TAIL)
+ 
---
+++ flip_step6.py
@@ -0,0 +1,16 @@
+import constant
+import random
+
+random.seed(10)
+
+print(random.random())
+random_val = random.random();
+if (random_val> 0.50):
+    print(constant.HEAD)
+else:
+    print(constant.TAIL)
+ 
+
+
+
+ 
